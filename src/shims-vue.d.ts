abcc2be968a633fc18c7d56e37b0a9decf5ba3f1
hahahashj <19818040549@163.com>
2025-12-07 11:28:10 +0800

    V3

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>
+  const component: DefineComponent<Record<string, any>, Record<string, any>, any>
   export default component
 }
